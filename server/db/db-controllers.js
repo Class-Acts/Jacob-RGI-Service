5953edd0d973deaf4b5181a23d5decb339300a18
Sam Quady <samquady1@gmail.com>
2021-01-26 13:43:38 -0800

    Patched DB Seeding, Updated ReadME

--- server/db/db-controllers.js
+++ server/db/db-controllers.js
@@ -1,19 +1,65 @@
-const mysql = require('mysql');
+const connection = require('./connection.js');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'reviews'
- });
+const connect = () => {
+  return connection.connect((err) => {
+    if (err) {
+      console.log('cannot connect to db', err);
+    } else {
+      console.log('connected to db');
+    }
+  });
+}
 
-connection.connect((err) => {
-  if (err) {
-    console.log('cannot connect to db');
-  } else {
-    console.log('connected to db!');
-  }
-})
+const dropDatabase = () => {
+  return connection.query(`DROP DATABASE reviews`);
+}
+
+const buildDatabase = () => {
+  return connection.query('CREATE DATABASE IF NOT EXISTS reviews');
+}
+
+const useDatabase = () => {
+  return connection.query(`USE reviews`);
+}
+
+const buildReviewsTable = () => {
+  return connection.query(`
+  CREATE TABLE IF NOT EXISTS users (
+
+    id smallint not null auto_increment,
+    name varchar(40) not null,
+    number_reviews tinyint not null,
+    typical_size varchar(20) not null,
+    height varchar(20) not null,
+    weight varchar(30) not null,
+    age varchar(30),
+    location varchar(50) not null,
+    primary key (id)
+    )
+  `);
+}
+
+const buildUsersTable = () => {
+  return connection.query(`
+  CREATE TABLE IF NOT EXISTS reviews (
+
+    id smallint not null auto_increment,
+    shoe_id smallint not null,
+    user_id smallint not null references users(id),
+    review_date date not null,
+    title varchar(200) not null,
+    body varchar(4000) not null,
+    stars tinyint not null,
+    fit tinyint not null,
+    width tinyint not null,
+    helpful tinyint not null,
+    not_helpful tinyint not null,
+    recommended tinyint(1) not null,
+    primary key (id)
+    )
+  `)
+}
+
 
 const truncateTable = (tablename, cb) => {
   connection.query('TRUNCATE ' + tablename, (err, results) => {
@@ -40,26 +86,32 @@ const insertData = (data, tablename, cb) => {
 
 
 const getReviewData = (shoeId, cb) => {
-  connection.query('SELECT * FROM reviews WHERE shoe_id = ' + shoeId, (err, results) => {
+  connection.query('USE reviews', (err) => {
     if (err) {
-      console.log('error querying the reviews table', err);
+      console.log('could not use reviews', err);
     } else {
-      console.log('success querying the reviews table');
-      let responseData = [];
-      let counter = 0;
-      responseData.push([results]);
-      results.forEach((review) => {
-        connection.query('SELECT * FROM users WHERE id = ' + review.user_id, (err, data) => {
-          if (err) {
-            console.log(err);
-          } else {
-            responseData.push(data);
-            counter ++
-            if (counter === results.length) {
-              cb(null, responseData);
-            }
-          }
-        })
+      connection.query('SELECT * FROM reviews WHERE shoe_id = ' + shoeId, (err, results) => {
+        if (err) {
+          console.log('error querying the reviews table', err);
+        } else {
+          console.log('success querying the reviews table');
+          let responseData = [];
+          let counter = 0;
+          responseData.push([results]);
+          results.forEach((review) => {
+            connection.query('SELECT * FROM users WHERE id = ' + review.user_id, (err, data) => {
+              if (err) {
+                console.log(err);
+              } else {
+                responseData.push(data);
+                counter ++
+                if (counter === results.length) {
+                  cb(null, responseData);
+                }
+              }
+            })
+          })
+        }
       })
     }
   })
@@ -67,6 +119,12 @@ const getReviewData = (shoeId, cb) => {
 
 
 module.exports = {
+  connect,
+  dropDatabase,
+  buildDatabase,
+  useDatabase,
+  buildReviewsTable,
+  buildUsersTable,
   truncateTable,
   insertData,
   getReviewData
