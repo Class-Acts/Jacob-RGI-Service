4187e50de0e001903100f6775e95bcffdf64bcdf
Sam Quady <samquady1@gmail.com>
2021-01-29 14:09:57 -0800

    Fixed Helpful Buttons, Added Size Text

--- client/review.jsx
+++ client/review.jsx
@@ -113,7 +113,7 @@ cursor: pointer;
 font-family: "Roboto","Helvetica Neue","Helvetica","Arial",sans-serif;
 font-size: 14px;
 font-weight: 400;
-height: 30px
+height: 34px;
 width: auto;
 &:hover {box-shadow: inset 0 0 3px #000000;}
 `;
@@ -251,6 +251,8 @@ class Review extends React.Component {
           </FitandWidthModule>
           <div>
             <ReviewBody>{this.props.review.body}</ReviewBody>
+            <UserBioBoldText>Typical Shoe Size</UserBioBoldText><UserBioDefaultText>{' ' + this.props.user.typical_size}</UserBioDefaultText>
+            <div></div>
             <UserBioBoldText>Height</UserBioBoldText><UserBioDefaultText>{' ' + this.props.user.height}</UserBioDefaultText>
             <div></div>
             <UserBioBoldText>Weight</UserBioBoldText><UserBioDefaultText>{' ' + this.props.user.weight}</UserBioDefaultText>
