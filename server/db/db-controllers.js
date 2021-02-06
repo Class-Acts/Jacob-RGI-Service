488714c6b9734d3fb098fb4514a651e4ec249d49
Jacob Eldred <jeldred95@gmail.com>
2021-02-05 23:28:28 -0800

    Refactored controller functions

--- server/db/db-controllers.js
+++ server/db/db-controllers.js
@@ -1,38 +1,15 @@
-const connection = require('./connection.js');
+const pool = require('./connection.js');
 
-//Logging functions
-const dontLog = () => null;
-const logVerbose = () => {
-
-}
-const logSpeed = (data, logtype, startTime) {
-
-}
-const logSimple = () => {
-  console.log('')
-}
-const log = (options, logtype) => {
-  // options argument requires 'result' property received from successful query or 'query' property if making a query attempt
-  if
-}
-const queryLog = (query) => {
-  console.log(``);
-}
-const resultLog = () => {
-  console.log(``);
-}
-const errorLog = () => {
-  console.error(``);
-}
-
-//Controllers
-const insertItem = (data, logging) => {
-  let text = `INSERT INTO items DEFAULT VALUES;`
+// Controllers
+const insertItem = (client) => {
+  const connection = client || pool;
+  const text = 'INSERT INTO items DEFAULT VALUES;';
   return connection.query(text);
-}
-insertReview
-const insertReview = (data, logging) => {
-  let values = [
+};
+
+const insertReview = (client, data) => {
+  const connection = client || pool;
+  const values = [
     data.item_id,
     data.user_id,
     data.date,
@@ -41,9 +18,9 @@ const insertReview = (data, logging) => {
     data.stars,
     data.fit,
     data.width,
-    data.recommend
-  ]
-  let text = `INSERT INTO reviews (
+    data.recommend,
+  ];
+  const text = `INSERT INTO reviews (
     item_id,
     user_id,
     date,
@@ -55,48 +32,52 @@ const insertReview = (data, logging) => {
     recommend
   ) VALUES (
     $1, $2, $3, $4, $5, $6, $7, $8, $9
-  );`
-  return connection.query(text, values)
-}
-
-const insertUser = (data, logging) => {
-  let values = [
-    data.item_id,
-    data.user_id,
-    data.date,
-    data.title,
-    data.body,
-    data.stars,
-    data.fit,
-    data.width,
-    data.recommend
-  ]
-  let text = `INSERT INTO reviews (
-    item_id,
-    user_id,
-    date,
-    title,
-    body,
-    stars,
-    fit,
-    width,
-    recommend
-  ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9
-  );`
-  return connection.query(text, values)
-}
-
-const insertFoundHelpful = (data, logging) => {
-  let values = [data.item_id, data.user_id];
-  let text = `INSERT INTO found_helpful (item_id, user_id) VALUES ($1, $2);`
+  );`;
   return connection.query(text, values);
-}
+};
 
+const insertUser = (client, data) => {
+  const connection = client || pool;
+  const values = [
+    data.name,
+    data.size,
+    data.height,
+    data.weight,
+    data.age,
+    data.location,
+  ];
+  const text = `INSERT INTO users (
+    name,
+    size,
+    height,
+    weight,
+    age,
+    location
+  ) VALUES (
+    $1, $2, $3, $4, $5, $6
+  );`;
+  return connection.query(text, values);
+};
+
+const insertFoundHelpful = (client, data) => {
+  const connection = client || pool;
+  const values = [data.review_id, data.user_id];
+  const text = 'INSERT INTO found_helpful (review_id, user_id) VALUES ($1, $2);';
+  return connection.query(text, values);
+};
+
+const getReviewData = (itemId, cb) => {
+  pool.query(`SELECT * FROM reviews WHERE id = ${itemId}`)
+    .then((result) => {
+      console.log(result);
+      cb(null, result);
+    });
+};
 
 module.exports = {
   insertItem,
   insertReview,
   insertUser,
   insertFoundHelpful,
-}
+  getReviewData,
+};
